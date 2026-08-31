0f7e0ad4b714a4ef43609df050c6a78927765375
CaiqueJosé <caiquejosedeoliveira@gmail.com>
2026-08-31 16:15:44 -0300

    mudar o css e html

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,92 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class CadastroForm(UserCreationForm):
-    email = forms.EmailField(required=True, label="E-mail")
-    first_name = forms.CharField(max_length=50, required=True, label="Nome")
-    last_name = forms.CharField(max_length=50, required=True, label="Sobrenome")
+
+    email = forms.EmailField(
+        required=True,
+        label="E-mail"
+    )
+
+    first_name = forms.CharField(
+        max_length=50,
+        required=True,
+        label="Nome"
+    )
+
+    last_name = forms.CharField(
+        max_length=50,
+        required=True,
+        label="Sobrenome"
+    )
+    
+    def clean_email(self):
+
+        email = self.cleaned_data["email"]
+
+        if User.objects.filter(
+            email__iexact=email
+        ).exists():
+
+            raise forms.ValidationError(
+                "Este e-mail já está cadastrado."
+            )
+
+        return email
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+        fields = [
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "password1",
+            "password2"
+        ]
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
+
 class LoginForm(AuthenticationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].label = 'Usuário ou E-mail'
-        self.fields['username'].widget.attrs.update({'autofocus': 'true'})
-        self.fields['password'].widget.attrs.update({'placeholder': 'Digite sua senha'})
+
+    username = forms.CharField(
+        label="Usuário ou E-mail",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Usuário ou e-mail",
+                "autofocus": True
+            }
+        )
+    )
+
+    password = forms.CharField(
+        label="Senha",
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+
+    def clean(self):
+
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+
+        if username and password:
+
+            try:
+                usuario = User.objects.get(
+                    email__iexact=username
+                )
+
+                username = usuario.username
+
+                self.cleaned_data["username"] = username
+
+            except User.DoesNotExist:
+                pass
+
+        return super().clean()
